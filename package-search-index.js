ddf1506405189163a630ab12ba00e39a266e7da5
TiJ-code <TiJ-code@users.noreply.github.com>
2026-08-16 17:09:06 +0000

    deploy: 09e4b6a2acd3464130638423c07ac29dbbbd0ce5

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"tij.jca.core.ids"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"tij.jca.core.ids"},{"l":"tij.jca.core.storage"},{"l":"tij.jca.core.storage.exceptions"},{"l":"tij.jca.core.storage.page"}];updateSearchResults();
